86f12098c9c890652b7190c0bfd01c18805e317f
Isa_oliveira <belefreiresls@outlook.com>
2023-09-11 19:47:08 -0300

    Create code.py

---
+++ code.py
@@ -0,0 +1,5 @@
+# Solicita ao usuário que insira seu nome
+nome = input("Digite seu nome: ")
+
+# Exibe uma saudação usando o nome fornecido
+print("Olá, " + nome + "! Bem-vindo à minha aplicação em Python.")
